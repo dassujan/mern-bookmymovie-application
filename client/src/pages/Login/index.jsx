08a94bcbad7c03d1a0a1a0b1f74034dc5cd24978
SUJAN DAS <dassujan.005@gmail.com>
2023-07-25 16:15:31 +0530

    user authentication front-end

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,13 +1,62 @@
-// Import the React object from the 'react' module.
-import React from "react";
+// Importing the necessary libraries and components
+import React from "react"; // Importing the React library, used for creating React components
+import { Form } from "antd"; // Importing the 'Form' component from the 'antd' library, a UI component library for React
+import Button from "../../components/Button"; // Importing a custom 'Button' component from a file located at '../../components/Button'
+import { Link } from "react-router-dom"; // Importing the 'Link' component from the 'react-router-dom' library, used for navigation in a React application
 
-// Define a new functional component called Login.
-function Login() {
-  // Return JSX representing the component's output.
-  // In this case, it returns a <div> element with the content "Login" inside it.
-  return <div>Login</div>;
+// Defining the 'LOGIN' functional component
+function LOGIN() {
+  // Creating a function 'onFinish' that will be called when the form is submitted successfully
+  const onFinish = (values) => {
+    console.log("Success:", values); // Output the form values to the console
+  };
+
+  // Rendering the component's UI using JSX
+  return (
+    <div className="flex justify-center h-screen items-center bg-primary">
+      {" "}
+      {/* Styling the container div */}
+      <div className="card p-3 w-400">
+        {" "}
+        {/* Styling the login card */}
+        <h1 className="text-xl mb-1"> BookMyMovie - LOGIN </h1>{" "}
+        {/* Displaying the heading */}
+        <hr /> {/* Horizontal line */}
+        <Form layout="vertical" className="mt-1" onFinish={onFinish}>
+          {" "}
+          {/* Creating a form with a vertical layout */}
+          {/* Form item for the 'Email' field */}
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <input type="email" /> {/* Input field for the email */}
+          </Form.Item>
+          {/* Form item for the 'Password' field */}
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <input type="password" /> {/* Input field for the password */}
+          </Form.Item>
+          <div className="flex flex-col mt-2 gap-1">
+            {" "}
+            {/* Styling the buttons and link in a flex column */}
+            <Button fullWidth title="LOGIN" type="submit" />{" "}
+            {/* Using the custom 'Button' component with the title "LOGIN" and submit type */}
+            <Link to="/register" className="text-primary">
+              {" "}
+              {/* Creating a link to the registration page */}
+              Don't have an account? Register {/* Text content for the link */}
+            </Link>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-// Export the Login component as the default export of this file.
-// This allows other files in the application to import and use this component.
-export default Login;
+// Exporting the 'LOGIN' component as the default export
+export default LOGIN;
--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,14 +1,71 @@
 // Importing the necessary libraries and components
 import React from "react"; // Importing the React library, used for creating React components
-import { Button } from "antd"; // Importing the 'Button' component from the 'antd' library, a UI component library for React
+import { Form } from "antd"; // Importing the 'Form' component from the 'antd' library, a UI component library for React
+import Button from "../../components/Button"; // Importing a custom 'Button' component from a relative file path
+import { Link } from "react-router-dom"; // Importing the 'Link' component from 'react-router-dom', used for navigation in React applications
 
 // Defining the 'Register' functional component
 function Register() {
+  // Callback function for form submission
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  // JSX code representing the UI of the 'Register' component
   return (
-    // Using JSX to return the component's UI
-    <div className="flex justify-center h-screen items-center">
-      {/* Rendering the 'Button' component with the type set to "primary" */}
-      <Button type="primary">Primary Button</Button>
+    // Outer container for the entire UI, applying styles for centering content and setting a background color
+    <div className="flex justify-center h-screen items-center bg-primary">
+      {/* Card-like container for the registration form, applying padding and setting width */}
+      <div className="card p-3 w-400">
+        {/* Heading for the registration form */}
+        <h1 className="text-xl mb-1">BookMyMovie - REGISTER</h1>
+        {/* Horizontal line to separate the heading from the form */}
+        <hr />
+
+        {/* Ant Design 'Form' component used for creating the registration form */}
+        <Form layout="vertical" className="mt-1" onFinish={onFinish}>
+          {/* Form field for user's name */}
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            {/* Text input field for the user's name */}
+            <input type="text" />
+          </Form.Item>
+
+          {/* Form field for user's email */}
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            {/* Text input field for the user's email */}
+            <input type="email" />
+          </Form.Item>
+
+          {/* Form field for user's password */}
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            {/* Text input field for the user's password */}
+            <input type="password" />
+          </Form.Item>
+
+          {/* Container for the 'REGISTER' button and the login link */}
+          <div className="flex flex-col mt-2 gap-1">
+            {/* Custom 'Button' component for user registration */}
+            <Button fullWidth title="REGISTER" type="submit" />
+
+            {/* 'Link' component for navigation to the login page */}
+            <Link to="/login" className="text-primary">
+              Already have an account? Login
+            </Link>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
